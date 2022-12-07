7c6b9cfe429d40d1994b3586d7bc81d7a96e2ffe
chimebukanian <chimebukanyanwu@gmail.com>
2022-12-07 18:43:11 +0100

    requirements to run app added and how to use

--- cc_gen/cc_gen/urls.py
+++ cc_gen/cc_gen/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-#   path('cc_gen/', include('ccgen.urls'))
 ]
+urlpatterns += [
+#    path('cc_gen/', include('ccgen.urls')
+    ]
