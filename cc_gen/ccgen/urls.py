a1a4110bb2aeffe3be146874ffc4b813831e07b2
Oluwatobi <amure387@gmail.com>
2022-12-09 13:27:49 +0000

    push

--- cc_gen/ccgen/urls.py
+++
@@ -1,8 +0,0 @@
-# Add Django site authentication urls (for login, logout, password management)
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-]
