2990da056b235dee70ae3ec9db97402b7ba3f6a2
Oluwatobi <amure387@gmail.com>
2022-12-10 05:48:07 +0000

    push

--- cc_gen/cc_gen/settings.py
+++ cc_gen/cc_gen/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for cc_gen project.
 
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'cc_gen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
