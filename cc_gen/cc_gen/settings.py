00ae4b92eded82dd4356c745225eb20016669e96
Oluwatobi <amure387@gmail.com>
2022-12-11 16:48:26 +0100

    push

--- cc_gen/cc_gen/settings.py
+++ cc_gen/cc_gen/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ccgen.apps.CcgenConfig'
+    'ccgen.apps.CcgenConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
