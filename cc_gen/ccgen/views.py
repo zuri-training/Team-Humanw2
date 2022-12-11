6f44fff8f7d0d7c376746cd48c86ce56bb98f59b
chimebukanian <chimebukanyanwu@gmail.com>
2022-12-11 19:08:36 +0100

    auth/signup

--- cc_gen/ccgen/views.py
+++ cc_gen/ccgen/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .forms import usersignupform
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
 # Create your views here.
+#@login_required
+def index(request):
+    return render(request, 'ccgen/home.html')
+
+
+class userCreateview(CreateView):
+    form_class=usersignupform
+    template_name='ccgen/usersignup.html'
+
+    def form_valid(self, form):
+        user=User.objects.create_user(email=self.request.POST['email'], username=self.request.POST['username'], password=self.request.POST['password'], )
+        return redirect('ccgen:index')
