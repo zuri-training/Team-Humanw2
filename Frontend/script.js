08e66ac25e5d171e96e157d8c8ee82d116013cec
Waris Adebayo <heinstein001@gmail.com>
2022-12-13 12:23:18 +0100

    fix the javascript issues

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,7 +1,10 @@
 const forgotPasswordModal = document.querySelector(".forgot-modal"),
-forgotPasswordForm = document.querySelector(".forgot-form");
+forgotPasswordForm = document.querySelector(".forgot-form"),
+loginVerifyModal = document.querySelector(".login-verify-modal");
+backToForgotPassword = document.querySelector(".to-forgot-password");
 
 // This function get the user mail then add it to the login verify modal
+console.log(backToForgotPassword)
 
 function showUserEmail() {
     let userEmail = document.querySelector("#recovery-email").value;
@@ -12,8 +15,14 @@ function showUserEmail() {
 
 forgotPasswordForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    document.querySelector('.login-verify-modal').style.display = "flex";
+    loginVerifyModal.style.display = "flex";
     forgotPasswordModal.style.display = "none";
 
     showUserEmail();
+})
+
+backToForgotPassword.addEventListener('click', () => {
+    loginVerifyModal.style.display = "none";
+    forgotPasswordModal.style.display = "flex";
+    console.log("It's working fine")
 })
