266d58e9659f9a4b0ecf0ca22cd0e24a3e727fdd
chimebukanian <chimebukaanyanwu@gmail.com>
2022-12-15 00:10:26 +0100

    uppdate

--- cc_gen/ccgen/urls.py
+++ cc_gen/ccgen/urls.py
@@ -4,6 +4,13 @@ from . import views
 app_name="ccgen"
 urlpatterns=[
     path('', views.index, name='index'),
-  
+    path('alldesigns/', views.designListView.as_view(), name='designs'),
+    path('design/<int:pk>', views.designDetailView.as_view(), name='design-detail'),
+    path('design/create/', views.designCreate.as_view(),name='design-create'),
+    path('design/<int:pk>/delete/', views.designDelete.as_view(), name='design-delete'),   
+    path('downloads/', views.downloadedListView.as_view(), name='downloads'),
+    path('saveddownloads/', views.savedfordownloadListView.as_view(), name='saved'),
+
+
     path('signup/', views.userCreateview.as_view(), name='usersignup'),
 ]
