0da8ece49d4f0c221e8b2a3394ce28bda2ab1ced
Oluwatobi <amure387@gmail.com>
2022-12-14 18:32:35 +0100

    push

--- cc_gen/cc_gen/settings.py
+++ cc_gen/cc_gen/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!4r!9w9h@@=t6r$io1s(og6917+v77%bw360$@gyz59rzu@4ir
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'templates')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- cc_gen/ccgen/views.py
+++ cc_gen/ccgen/views.py
@@ -1,18 +1,2 @@
 from django.shortcuts import render
 
-from django.view.generic import TemplateView
-
-class SignUpView(TemplateView):
-	template_name = 'signup.html'
-class HomeView(TemplateView):
-	template_name = 'home.html'
-class LoginView(TemplateView):
-	template_name = 'login.html'
-class LogoutView(TemplateView):
-	template_name ='logged_out.html'
-class PassEmailView(TemplateView):
-	template_name = 'password_reset_email.html'
-class PassFormView(TemplateView):
-	template_name = 'password_reset_email.html'
-class PassCompleteView(TemplateView):
-	template_name = 'password_reset_complete.html'
